d3fca4243d7715587dc693c2c277f3a9513b32bd
jingjiezhuang <jingjie.zhuang@igg.com>
2022-05-19 21:32:07 +0800

    Add virtual_touchscreen

--- container-bootstrap.sh
+++ container-bootstrap.sh
@@ -2,4 +2,8 @@
 
 echo "+cpuset" >/sys/fs/cgroup/cgroup.subtree_control
 chmod go+rx /run/user/1000
+if [ ! -f "/dev/virtual_touchscreen" ]; then
+  insmod /usr/src/virtual_touchscreen/virtual_touchscreen.ko abs_x_max=2560 abs_y_max=1440
+  chmod 666 /dev/virtual_touchscreen
+fi
 export PULSE_SYSTEM=1
